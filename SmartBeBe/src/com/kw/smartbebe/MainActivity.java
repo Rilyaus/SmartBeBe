465b4903fe2d07f2f82b2aac31d6ca8b7b428e55
Rilyaus <k-426kgd@nate.com>
2014-09-19 17:35:05 +0900

    App Icon Modify, Navigation Drawer Modify, Icon File Add

--- SmartBeBe/src/com/kw/smartbebe/MainActivity.java
+++ SmartBeBe/src/com/kw/smartbebe/MainActivity.java
@@ -18,8 +18,8 @@ public class MainActivity extends FragmentActivity implements OnClickListener {
 	private LinearLayout navi_lineaLayout;
 	private ListView navi_listview1, navi_listview2;
 	private Button navi_toggle_btn;
-	private ArrayList<Navigation_MenuList> naviMenuArr;
-	private NaviMenuListAdapter nAdapter;
+	private ArrayList<Navigation_MenuList> naviMenuArr1, naviMenuArr2;
+	private NaviMenuListAdapter nAdapter1, nAdapter2;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +29,10 @@ public class MainActivity extends FragmentActivity implements OnClickListener {
 		navi_toggle_btn = (Button)findViewById(R.id.navi_toggle_btn);
 		navi_lineaLayout = (LinearLayout)findViewById(R.id.navi_linearlayout);
 		navi_listview1 = (ListView)findViewById(R.id.navi_menulist1);
-		
-		naviMenuArr = new ArrayList<Navigation_MenuList>();
+		navi_listview2 = (ListView)findViewById(R.id.navi_menulist2);
+
+		naviMenuArr1 = new ArrayList<Navigation_MenuList>();
+		naviMenuArr2 = new ArrayList<Navigation_MenuList>();
 		
 		Paint paint = new Paint();
 		paint.setARGB(0, 214, 245, 255);;
@@ -62,17 +64,19 @@ public class MainActivity extends FragmentActivity implements OnClickListener {
 	}
 
 	public void setNaviMenu() {
-		naviMenuArr.add(new Navigation_MenuList(getResources().getString(R.string.menu_diary), getResources().getDrawable(R.drawable.diary_128x128)));
-		naviMenuArr.add(new Navigation_MenuList(getResources().getString(R.string.menu_vaccine), getResources().getDrawable(R.drawable.vaccine_128x128)));
-		naviMenuArr.add(new Navigation_MenuList(getResources().getString(R.string.menu_babyfood), getResources().getDrawable(R.drawable.food_128x128)));
-		naviMenuArr.add(new Navigation_MenuList(getResources().getString(R.string.menu_growth), getResources().getDrawable(R.drawable.growth_128x128)));
-		
-		nAdapter = new NaviMenuListAdapter(this, R.layout.navi_menulist_listitem, naviMenuArr);
-		
-		navi_listview1.setAdapter(nAdapter);
-		navi_listview1.setOnItemClickListener(new OnItemClickListener() {
+		naviMenuArr1.add(new Navigation_MenuList(getResources().getString(R.string.menu_diary), getResources().getDrawable(R.drawable.diary_128x128)));
+		naviMenuArr1.add(new Navigation_MenuList(getResources().getString(R.string.menu_vaccine), getResources().getDrawable(R.drawable.vaccine_128x128)));
+		naviMenuArr1.add(new Navigation_MenuList(getResources().getString(R.string.menu_babyfood), getResources().getDrawable(R.drawable.food_128x128)));
+		naviMenuArr1.add(new Navigation_MenuList(getResources().getString(R.string.menu_growth), getResources().getDrawable(R.drawable.growth_128x128)));
 
-			@Override
+		naviMenuArr2.add(new Navigation_MenuList(getResources().getString(R.string.menu_mypage), getResources().getDrawable(R.drawable.mypage_128x128)));
+		naviMenuArr2.add(new Navigation_MenuList(getResources().getString(R.string.menu_setting), getResources().getDrawable(R.drawable.setting_128x128)));
+
+		nAdapter1 = new NaviMenuListAdapter(this, R.layout.navi_menulist_listitem, naviMenuArr1);
+		nAdapter2 = new NaviMenuListAdapter(this, R.layout.navi_menulist_listitem, naviMenuArr2);
+
+		navi_listview1.setAdapter(nAdapter1);
+		navi_listview1.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> av, View v, int position, long id) {
 				dlDrawer.closeDrawer(navi_lineaLayout);
 				
@@ -89,6 +93,13 @@ public class MainActivity extends FragmentActivity implements OnClickListener {
 				ft.addToBackStack("HomeTab");
 				ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE);
 				ft.commitAllowingStateLoss();*/
+			}
+		});
+	
+		navi_listview2.setAdapter(nAdapter2);
+		navi_listview2.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> av, View v, int position, long id) {
+				dlDrawer.closeDrawer(navi_lineaLayout);
 			}
 		});
 	}
